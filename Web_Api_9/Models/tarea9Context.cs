fdefab2d98d2ea735e97d6319ab1c25767fe2ff3
juniorulloa23 <59671640+juniorulloa23@users.noreply.github.com>
2021-08-08 19:04:47 -0400

    hosting

--- Web_Api_9/Models/tarea9Context.cs
+++ Web_Api_9/Models/tarea9Context.cs
@@ -24,7 +24,7 @@ namespace Web_Api_9.Models
             if (!optionsBuilder.IsConfigured)
             {
 // #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;user=root;database=tarea9", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.18-mysql"));
+                optionsBuilder.UseMySql("Server=apiweb9.mysql.database.azure.com; Port=3306; Database=tarea9; Uid=rooter@apiweb9; Pwd=internet@23; SslMode=Preferred;", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.18-mysql"));
             }
         }
 
